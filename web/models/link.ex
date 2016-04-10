6c0a6e7d4db7923c35a29b8c5ee61c7dbb9f691a
Jingjing Duan <jduan@fitbit.com>
2016-04-10 09:20:59 -0700

    validate URLs

--- web/models/link.ex
+++ web/models/link.ex
@@ -1,4 +1,5 @@
 defmodule GoLinks.Link do
+  require Logger
   use GoLinks.Web, :model
 
   schema "links" do
@@ -21,5 +22,29 @@ defmodule GoLinks.Link do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_change(:url, fn
+      :url, url ->
+        if valid_url?(url) do
+          Logger.debug "url is valid"
+          []
+        else
+          Logger.debug "url is invalid"
+          [url: "url #{url} is invalid2"]
+        end
+    end)
+  end
+
+  defp valid_url?(url) do
+    uri = URI.parse(url)
+    Logger.debug "uri is #{inspect uri}"
+    case uri do
+      %URI{scheme: nil} -> false
+      %URI{host: nil} -> false
+      uri -> case uri do
+        %URI{scheme: "http"} -> true
+        %URI{scheme: "https"} -> true
+        _ -> false
+      end
+    end
   end
 end
