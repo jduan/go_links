a3b2285e5c50bd385aac1bfadd3ce0ee5f55d1e2
Jingjing Duan <jduan@fitbit.com>
2016-04-26 23:17:53 -0700

    add redirect_controller_test

---
+++ test/controllers/redirect_controller_test.exs
@@ -0,0 +1,13 @@
+defmodule GoLinks.RedirectControllerTest do
+  use GoLinks.ConnCase
+
+  # alias GoLinks.Link
+  @valid_attrs %{name: "some content", url: "https://www.google.com"}
+  @invalid_attrs %{name: ""}
+
+  test "should receive 404 'Page not found' for non-existent link", %{conn: conn} do
+    # conn = get conn, redirect_path(conn, :handle_redirect, "badlink")
+    conn = get conn, "/badlink"
+    assert html_response(conn, 404) =~ "Page not found"
+  end
+end
